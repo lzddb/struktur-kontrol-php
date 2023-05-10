fe058df3e62b07ab1dc2d8afcc06d96fafa5696b
lzddb <attafanif@gmail.com>
2023-05-10 11:42:13 +0700

    Program Sederhana Struktur Kontrol PHP

---
+++ tugas1.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>TUGAS 1</title>
+<body>
+<?php
+$minimal_belanja = 100000;
+$total_belanja = 120000;
+
+if ($total_belanja > $minimal_belanja) {
+  $diskon = $total_belanja * 0.1;
+  $total_bayar = $total_belanja - $diskon;
+  echo "Total belanja Anda adalah Rp. " . $total_belanja . ", mendapatkan diskon 10% sebesar Rp. " . $diskon . ", sehingga total yang harus dibayar adalah Rp. " . $total_bayar . ".";
+} else {
+  echo "Total belanja Anda adalah Rp. " . $total_belanja . ", belum mencapai minimal belanja sebesar Rp. " . $minimal_belanja . ".";
+}
+?>
+
+</body>
+</html>
+
---
+++ tugas2.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title>TUGAS 2</title>
+<body>
+<?php
+$minimal_belanja = 100000;
+$total_belanja = 100000;
+
+if ($total_belanja >= $minimal_belanja) {
+  $diskon = $total_belanja * 0.1;
+  $total_bayar = $total_belanja - $diskon;
+  echo "Total belanja Anda adalah Rp. " . $total_belanja . ", mendapatkan diskon 10% sebesar Rp. " . $diskon . ", sehingga total yang harus dibayar adalah Rp. " . $total_bayar . ".";
+} else {
+  echo "Total belanja Anda adalah Rp. " . $total_belanja . ", belum mencapai minimal belanja sebesar Rp. " . $minimal_belanja . ".";
+}
+
+?>
+
+</body>
+</html>
+
